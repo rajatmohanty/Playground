a6f076515d3f0bfaf4994d99f328d0b716b01510
Kuixi Song <krayc425@gmail.com>
2018-04-09 00:14:59 +0800

    Modify Visitor Pattern

--- SwiftPlayground/VisitorPattern.playground/Contents.swift
+++ SwiftPlayground/VisitorPattern.playground/Contents.swift
@@ -2,17 +2,28 @@ import Foundation
 
 protocol Visitor {
     func visit(element: MyElement)
+    // Or you can write both
+    //     func visit(element: ElementA)
+    // and
+    //     func visit(element: ElementB)
+    func visit(element: UniqueElement)
 }
 
 class VisitorOne: Visitor {
     func visit(element: MyElement) {
         element.printInfo()
     }
+    func visit(element: UniqueElement) {
+        print("I am visiting a unique element.")
+    }
 }
 
 class VisitorTwo: Visitor {
     func visit(element: MyElement) {
-        print("Who is this? This is \(element.elementName)")
+        print("Who is this? This is \(element.elementName).")
+    }
+    func visit(element: UniqueElement) {
+        print("I am visiting a unique element, too.")
     }
 }
 
@@ -22,7 +33,7 @@ class MyElement {
         visitor.visit(element: self)
     }
     func printInfo() {
-        print(elementName)
+        print(elementName, terminator: ".\n")
     }
 }
 
@@ -54,24 +65,34 @@ extension Array where Element: MyElement {
     }
 }
 
-print("----------Begin----------")
+//  UniqueElement doesn't inherit from MyElement
+class UniqueElement {
+    func accept(visitor: Visitor) {
+        visitor.visit(element: self)
+    }
+}
+
+print("-------------Begin!-------------")
 
 let elementA = ElementA()
 let elementB = ElementB()
+let uniqueElement = UniqueElement()
 
-print("----------Visitor1----------")
+print("------------Visitor1------------")
 
 let visitor1 = VisitorOne()
 elementA.accept(visitor: visitor1)
 elementB.accept(visitor: visitor1)
+uniqueElement.accept(visitor: visitor1)
 
-print("----------Visitor2----------")
+print("------------Visitor2------------")
 
 let visitor2 = VisitorTwo()
 elementA.accept(visitor: visitor2)
 elementB.accept(visitor: visitor2)
+uniqueElement.accept(visitor: visitor2)
 
-print("----------VisitorArray----------")
+print("---------MyElementArray---------")
 
 let elementArray = [ElementA(), ElementB()]
 elementArray.accept(visitor: visitor1)
